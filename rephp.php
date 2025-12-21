53ceb497563c19c984f07c2bcc7cd55680eda886
CREATOR4k <131431479+CREATOR4k@users.noreply.github.com>
2025-12-21 18:03:57 +0300

    Delete rephp.php

--- rephp.php
+++
@@ -1,6 +0,0 @@
- // пример подгрузки ошибок из PHP
-    fetch('init.php')
-      .then(r => r.text())
-      .then(data => {
-        document.getElementById('login-error').innerHTML = data;
-      });
